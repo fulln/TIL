a1d295a3bd7c10a55ad7f93711dfb1353c78aa28
fulln <245193247@qq.com>
2020-07-27 19:52:57 +0800

    update test

--- update_readme.py
+++ update_readme.py
@@ -16,12 +16,12 @@ if __name__ == "__main__":
     db = sqlite_utils.Database(root / "til.db")
     by_topic = {}
     for row in db["til"].rows_where(order_by="created_utc"):
-        by_topic.setdefault(".".join(row["topic"]), []).append(row)
+        by_topic.setdefault(str(row["topic"]), []).append(row)
     index = ["<!-- index starts -->"]
     for topic, rows in by_topic.items():
         sharp = '##'
-        topic = topic.split(".")
-        for i in range(len(topic) - 1):
+        topic = json.loads(topic)
+        for i in range(len(topic)):
             index.append("{} {}\n".format(sharp,topic[i]))
             sharp = sharp +'#'
         for row in rows:
